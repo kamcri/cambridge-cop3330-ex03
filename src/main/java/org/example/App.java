1e20b77ad13472768b82b8bd879be1857b4537c2
kamcri <89857199+kamcri@users.noreply.github.com>
2021-09-08 17:55:24 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 first_name last_name
+ */
+package org.example;
+import java.util.Scanner;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner str = new Scanner(System.in);
+        System.out.println("What is the quote?");
+        String quote = str.nextLine();
+
+        Scanner str2 = new Scanner(System.in);
+        System.out.println("Who said it?");
+        String author = str2.nextLine();
+
+        System.out.println(author + " says, " + '"' + quote + '"');
+
+        str.close();
+        str2.close();
+    }
+}
